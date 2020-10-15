50021206aba3a5d14e97173c843717ec220d7092
Amit Mehta <amit.mehta@tatvasoft.com>
2020-10-15 10:38:17 +0530

    User Controller, model and Seeded data

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,47 @@
+module Api
+    class UsersController < ApplicationController
+        before_action :check_user, only: [:show, :update, :destroy]
+
+        #GET /api/users - returns all users from users table
+        def index
+
+        end
+
+        #GET /api/user/:id - returns specific user from users table given id
+        def show
+
+        end
+
+        #POST /api/user - adds a new user to the users table
+        def create
+            @user = User.new(user_params)
+            if @user.save
+                render json: { user: @user, message: "User created successfully."}, status: :created
+            else
+                render json: @user.errors, status: :unprocessable_entity
+            end
+        end
+
+        #PUT /api/user/:id - updates specific user in users table
+        def update
+
+        end
+
+        #DELETE /api/user/:id - removes the specific user from users table with the given id
+        def destroy
+
+        end
+
+        private
+        #only allow a trusted parameters 'white list' through
+        def user_params
+            params.require(:user).permit(:firstName, :lastName, :email)
+        end
+
+        #Check if user does not exists in database
+        def check_user
+            @user = User.find(params[:id])
+            return render json: { message: "Record not found."} unless @user.present?
+        end
+    end
+end
